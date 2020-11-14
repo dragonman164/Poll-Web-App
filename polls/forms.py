43fa567297a1ec2cff080229435b2ca3fae072f1
dragonman164 <sanidhiyafirefox123@gmail.com>
2020-11-14 16:14:51 +0530

    Added Options

--- polls/forms.py
+++ polls/forms.py
@@ -4,11 +4,12 @@ from .models import Question
 class QuestionForm(forms.ModelForm):
     class Meta:
         model = Question
-        fields = ['question','no_of_options']
+        fields = ['question','option1','option2','option3','option4']
         widgets = {
             'question':forms.TextInput(attrs= {'class':'form-control'}),
-            'no_of_options':forms.NumberInput(attrs= {'class':'form-control'}),
-
-            
+            'option1':forms.TextInput(attrs ={'class':'form-control'}),
+            'option2':forms.TextInput(attrs ={'class':'form-control'}),
+            'option3':forms.TextInput(attrs ={'class':'form-control'}),
+            'option4':forms.TextInput(attrs ={'class':'form-control'}),        
         }
 
--- polls/models.py
+++ polls/models.py
@@ -3,8 +3,12 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
     question = models.CharField(max_length=200,blank=False)
-    no_of_options = models.IntegerField(default=0)
     pub_date = models.DateTimeField(auto_now=True)
+    option1 = models.CharField(max_length=200,default="Option 1",blank=False)
+    option2 = models.CharField(max_length=200,default="Option 2",blank=False)
+    option3 = models.CharField(max_length=200,default="Option 3",blank=False)
+    option4 = models.CharField(max_length=200,default="Option 4",blank=False)
+    
     
     def __str__(self):
         return self.question
